0b37778d55f4824b07bb83677fb2ad1145859c1c
Benyamin Zojaji <benyamin.zojaji@gmail.com>
2024-03-15 19:35:56 +0330

    change pattern path

--- Assignment02/AI_image_plant_identifier/text2image.py
+++ Assignment02/AI_image_plant_identifier/text2image.py
@@ -6,7 +6,7 @@ class Illusion_diffusion:
     def __init__(self, Fal_API_KEY, prompt):
         self.url = "https://54285744-illusion-diffusion.gateway.alpha.fal.ai/"
         self.payload = {
-            "image_url": "https://github.com/BenyaminZojaji/PyDeploy-Course/blob/main/Assignment02/pattern.png?raw=true",
+            "image_url": "https://github.com/BenyaminZojaji/PyDeploy-Course/blob/main/Assignment02/AI_image_plant_identifier/assets/pattern.png?raw=true",
             "prompt": f"(masterpiece:1.4), (best quality), (detailed), {prompt}",
             "negative_prompt": "(worst quality, poor details:1.4), lowres, (artist name, signature, watermark:1.4), bad-artist-anime, bad_prompt_version2, bad-hands-5, ng_deepnegative_v1_75t"
         }
