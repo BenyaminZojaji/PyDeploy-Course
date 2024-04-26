043e1cebeb68e65eb7459f53e7964bb64f5f7a02
Benyamin Zojaji <benyamin.zojaji@gmail.com>
2024-04-26 23:25:12 +0330

    Update crud.py
    add delete, update function for course and students + fix minor bug

--- Assignment06/expert_app/app/crud.py
+++ Assignment06/expert_app/app/crud.py
@@ -18,6 +18,7 @@ def create_student(db: Session, student: schemas.StudentCreate):
     db.refresh(db_student)
     return db_student
 
+
 def get_course(db: Session, course_id: int):
     return db.query(models.Course).filter(models.Course.id == course_id).first()
 
@@ -33,10 +34,64 @@ def create_course(db: Session, course: schemas.CourseCreate):
     db.refresh(db_course)
     return db_course
 
-def add_course_to_student(db: Session, student_id: int, course_id: int): # TODO: need fix
+
+def delete_student(db: Session, student_id: int):
+    student = db.query(models.Student).filter(models.Student.id == student_id).first()
+    if student is None:
+        return False
+    else:
+        db.delete(student)
+        db.commit()
+        return True
+
+
+def delete_course(db: Session, course_id: int):
+    course = db.query(models.Course).filter(models.Course.id == course_id).first()
+    if course is None:
+        return False
+    else:
+        db.delete(course)
+        db.commit()
+        return True
+
+
+def update_student(db: Session, student_id: int, student: schemas.StudentCreate):
+    db_student = db.query(models.Student).filter(models.Student.id == student_id).first()
+    if db_student is None:
+        return False
+    db_student.firstname = student.firstname
+    db_student.lastname = student.lastname
+    db_student.average = student.average
+    db_student.graduated = student.graduated
+    db.commit()
+    db.refresh(db_student)
+    return db_student
+
+
+def update_course(db: Session, course_id: int, course: schemas.CourseCreate):
+    db_course = db.query(models.Course).filter(models.Course.id == course_id).first()
+    if db_course is None:
+        return False
+    db_course.name = course.name
+    db_course.unit = course.unit
+    db.commit()
+    db.refresh(db_course)
+    return db_course
+
+
+def add_course_to_student(db: Session, student_id: int, course_id: int):
     student = db.query(models.Student).get(student_id)
     course = db.query(models.Course).get(course_id)
     
     student.courses.append(course)
-    course.students.append(student)
+    # course.students.append(student)
+    db.commit()
+
+
+def delete_course_from_student(db: Session, student_id: int, course_id: int):
+    student = db.query(models.Student).get(student_id)
+    course = db.query(models.Course).get(course_id)
+    
+    student.courses.remove(course)
+    # course.students.remove(student)
     db.commit()
